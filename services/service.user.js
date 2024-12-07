821b2b0adb89fdd171713880c5dc43de4a6b7f6d
gitpeters <abraham.p.asuquo@gmail.com>
2024-12-07 22:27:47 +0100

    clean up user service

--- services/service.user.js
+++ services/service.user.js
@@ -3,46 +3,51 @@ const AppException = require('../exceptions/exception.app');
 const dateFormatter = require('../utils/date.formatter');
 const UserRepository = require('../database/database.user.repository');
 
-exports.createUser = async data => {
-  const userModel = new User(
-    undefined,
-    data.firstName,
-    data.lastName,
-    data.email
-  );
-  return await UserRepository.save(userModel);
-};
-
-exports.findAllUsers = async () => {
-  return await UserRepository.findAll();
-};
-
-exports.findByUser = async id => {
-  const user = await UserRepository.findById(id);
-  if (!user) {
-    throw new AppException('No user record found', 404);
+class UserService {
+  constructor() {}
+  async createUser(data) {
+    const userModel = new User(
+      undefined,
+      data.firstName,
+      data.lastName,
+      data.email
+    );
+    return await UserRepository.save(userModel);
   }
-  return user;
-};
 
-exports.updateUser = async (id, data) => {
-  const dbUser = await UserRepository.findById(id);
-  const user = mapUserTo(dbUser[0]);
-  const updatedRecord = { ...user, ...data };
-  return await UserRepository.updateById(id, updatedRecord);
-};
+  async findAllUsers() {
+    return await UserRepository.findAll();
+  }
 
-exports.deleteUser = async id => {
-  await UserRepository.deleteById(id);
-};
+  async findByUser(id) {
+    const user = await UserRepository.findById(id);
+    if (!user) {
+      throw new AppException('No user record found', 404);
+    }
+    return user;
+  }
 
-const mapUserTo = record => {
-  return {
-    id: record.id,
-    firstName: record.first_name,
-    lastName: record.last_name,
-    email: record.email,
-    createdAt: record.created_at,
-    updatedAt: dateFormatter(Date.now()),
-  };
-};
+  async updateUser(id, data) {
+    const dbUser = await UserRepository.findById(id);
+    const user = mapUserTo(dbUser[0]);
+    const updatedRecord = { ...user, ...data };
+    return await UserRepository.updateById(id, updatedRecord);
+  }
+
+  async deleteUser(id) {
+    await UserRepository.deleteById(id);
+  }
+
+  mapUserTo(record) {
+    return {
+      id: record.id,
+      firstName: record.first_name,
+      lastName: record.last_name,
+      email: record.email,
+      createdAt: record.created_at,
+      updatedAt: dateFormatter(Date.now()),
+    };
+  }
+}
+
+module.exports = new UserService();
